612f38cbcb63ccd6a238111dd699e2321dc737d7
EliasDAraneta <juanelias.araneta@smc.pshs.edu.ph>
2026-01-09 10:00:35 +0800

    Convert steps to numpy array
    Changed np.steps to a numpy array for better data handling.

--- [Araneta]_Q3_FA02.py
+++ [Araneta]_Q3_FA02.py
@@ -1,11 +1,12 @@
 import numpy as np
 
 names = ["Me", "Lia", "Jake"]
-np.steps = [
+steps = np.array([
   [4500, 5200, 4800, 5000, 5300],
   [4000, 4100, 3900, 4200, 4600],
   [6000, 5800, 5900, 6100, 6200]
-]
+])
 
 for i in range(len(names)):
+
     print(names[i], np.steps[i])
